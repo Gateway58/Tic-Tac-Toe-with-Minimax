62ff8a7ed999ef79ca6eb423dcc4fb7ef61cfc4f
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-04-06 13:12:58 +0200

    Update include.hpp

--- include.hpp
+++ include.hpp
@@ -1,1 +1,18 @@
+#pragma once
 
+//This hpp file will be includet in pretty much every cpp file of mine, because it takes so long to just write that out every time i create something
+//I will link this file in the cases i use this 
+#include <iostream>
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <math.h>
+#include<fstream>
+#include <ostream>
+#include <istream>
+#include <string>
+#include <Windows.h>
+#include<string.h>
+#include <sstream>
+#include <conio.h>
